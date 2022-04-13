e74bc0e825692b260d226b26396e9241dbc62917
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-12 22:42:59 -0300

    :pushpin: adicionando ViewModel
    add

---
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/view/AllCharactersFragment.kt
@@ -0,0 +1,8 @@
+package com.br.leandro.desafio_mobile.presenter.view
+
+import androidx.fragment.app.Fragment
+
+class AllCharactersFragment : Fragment() {
+
+    private val viewModel: AllCharactersViewModel by viewModel()
+}
---
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/viewmodel/AllCharactersViewModel.kt
@@ -0,0 +1,6 @@
+package com.br.leandro.desafio_mobile.presenter.viewmodel
+
+import androidx.lifecycle.ViewModel
+import java.util.concurrent.Flow
+
+class AllCharactersViewModel
