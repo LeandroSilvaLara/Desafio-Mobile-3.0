a7ab315203ff4f91f8e4f1758efa209b129d5461
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-13 10:18:33 -0300

    :package: atualizando navation
    nav

---
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/view/CharacterDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.br.leandro.desafio_mobile.presenter.view
+
+import androidx.fragment.app.Fragment
+
+class CharacterDetailsFragment : Fragment() {
+    val args: CharacterDetailsFragmentArgs by novArgs()
+}
