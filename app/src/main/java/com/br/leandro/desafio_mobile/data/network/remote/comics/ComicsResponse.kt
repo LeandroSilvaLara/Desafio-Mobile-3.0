e0edeba31daac522f21382b5c8511a4f0113ec6c
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-13 07:38:16 -0300

    :wrench: MarvelService
    MarvelService

---
+++ app/src/main/java/com/br/leandro/desafio_mobile/data/network/remote/comics/ComicsResponse.kt
@@ -0,0 +1,7 @@
+package com.br.leandro.desafio_mobile.data.network.remote.comics
+
+data class ComicsResponse (
+    val code: Int,
+    val data: Data,
+    val status: String
+)
---
+++ app/src/main/java/com/br/leandro/desafio_mobile/data/network/remote/comics/Data.kt
@@ -0,0 +1,6 @@
+package com.br.leandro.desafio_mobile.data.network.remote.comics
+
+data class Data (
+    val date: String,
+    val type: String
+)
