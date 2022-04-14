542ba01f3ca0784e3564982001a5d1d0739184a1
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-13 23:09:24 -0300

    :construction: MarvelYoutubeViewModel
    MarvelYoutubeViewModel

--- app/src/main/java/com/br/leandro/desafio_mobile/presenter/view/MarvelYoutubeActivity.kt
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/view/MarvelYoutubeActivity.kt
@@ -1,8 +1,15 @@
 package com.br.leandro.desafio_mobile.presenter.view
 
 import androidx.appcompat.app.AppCompatActivity
+import com.br.leandro.desafio_mobile.databinding.ActivityMarvelYoutubeBinding
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.reward.RewardedVideoAd
 
 class MarvelYoutubeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMarvelYoutubeBinding
+    private lateinit var interstitialAd: InterstitialAd
+    private lateinit var rewardedVideoAd: RewardedVideoAd
+    private val viewModelYoutube: MarvelYoutubeViewModel by viewModel()
 
 }
---
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/viewmodel/MarvelYoutubeViewModel.kt
@@ -0,0 +1,24 @@
+package com.br.leandro.desafio_mobile.presenter.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MarvelYoutubeViewModel (
+    private val getVideosUseCase: GetVideosUseCase
+    ) : ViewModel() {
+
+        private val _videosLiveData = MutableLiveData<List<VideoUiModel>>()
+        val videos = _videosLiveData as LiveData<List<VideoUiModel>>
+
+        fun getVideos() {
+            viewModelScope.launch {
+                val videoList = getVideosUseCase()
+                _videosLiveData.value = videoList.map { video ->
+                    video.toVideoUiModel()
+                }
+            }
+        }
+}
