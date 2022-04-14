f5c2631a833122a22084471705608a3f083d6b32
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-14 08:48:04 -0300

    :construction: AllCharactersFragment
    add

--- app/src/main/java/com/br/leandro/desafio_mobile/presenter/view/AllCharactersFragment.kt
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/view/AllCharactersFragment.kt
@@ -1,8 +1,15 @@
 package com.br.leandro.desafio_mobile.presenter.view
 
+import android.net.ConnectivityManager
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.br.leandro.desafio_mobile.presenter.viewmodel.AllCharactersViewModel
 
 class AllCharactersFragment : Fragment() {
 
     private val viewModel: AllCharactersViewModel by viewModel()
+    private val networkChecker by lazy {
+        NetworkChecker(ContextCompat.getSystemService(requireContext(), ConnectivityManager::class.java))
+    }
+
 }
