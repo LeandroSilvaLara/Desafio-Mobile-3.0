481d6eb29bf19ac9b83dd7a4c286a62c5468919b
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-16 14:02:31 -0300

    :recycle: Utils
    Utils

---
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.br.leandro.desafio_mobile.presenter.utils
+
+
+class Constants {
+    companion object {
+        const val VOICE_REQUEST_CODE = 10
+    }
+}
---
+++ app/src/main/java/com/br/leandro/desafio_mobile/presenter/utils/ReviewListConverter.kt
@@ -0,0 +1,21 @@
+package com.br.leandro.desafio_mobile.presenter.utils
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+
+@Suppress("unused")
+open class ReviewListConverter {
+    @TypeConverter
+    fun fromString(value: String): List<Review>? {
+        val listType = object : TypeToken<List<Review>>() {}.type
+        return Gson().fromJson<List<Review>>(value, listType)
+    }
+
+    @TypeConverter
+    fun fromList(list: List<Review>?): String {
+        val gson = Gson()
+        return gson.toJson(list)
+    }
+}
